3b30a21526661a1bfb71220b07b34f7e135477a0
Phyllis Laryea <plaryea@zeepayghana.com>
2026-06-19 16:52:05 +0000

    cat project

---
+++ capstone_projects/cat_facts/index.js
@@ -0,0 +1,51 @@
+import express from "express";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+const MY_URL = "https://cataas.com/"
+
+app.use(express.static("public"));
+
+app.get("/", (req,res) =>{
+    res.render("index.ejs");
+})
+
+app.get("/cats", (req,res) =>{
+    res.render("cats.ejs");
+})
+
+// display random picture of cat
+app.get("/random-cat", async (req, res) => {
+     try {
+    const catImageUrl = `${MY_URL} + "/cat"`;
+    res.render("cats.ejs" , {catImage: catImageUrl});
+  } catch (error) {
+      res.render("cats.ejs", { catImage:null, error: "No cat available 😿" });
+    }
+});
+
+app.get("/random-saying", async (req, res) => {
+  const searchId = req.body.id;
+     try {
+    const result = await axios.get(API_URL + "/cat");
+    res.render("index.ejs" , {content: JSON.stringify(result.data)});
+  } catch (error) {
+      res.render("index.ejs", { content: JSON.stringify(error.response.data) });
+    }
+});
+
+app.get("/filter-cats", async (req, res) => {
+  const searchId = req.body.id;
+     try {
+    const result = await axios.get(API_URL + "/secrets/" + searchId, config);
+    res.render("index.ejs" , {content: JSON.stringify(result.data)});
+  } catch (error) {
+      res.render("index.ejs", { content: JSON.stringify(error.response.data) });
+    }
+});
+
+app.listen(port, () =>{
+    console.log(`Server is running on port ${port}`);
+});
+
